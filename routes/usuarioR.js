07a5d26bebac1faf5693c1dec309019cd38490e6
Delfina Orlansky <dorlansky@udesa.edu.ar>
2020-05-18 13:21:28 -0300

    cambie comentario en la ruta de usuario

--- routes/usuarioR.js
+++ routes/usuarioR.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-//esto va  a --> localhost:3000/usuarios
+//esto va  a --> localhost:3000/usuario
 
 const peliculasController = require('../controllers/peliculasController');
 router.get("/", peliculasController.index);
