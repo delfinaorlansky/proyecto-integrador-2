34abdf26dd73d69fefd297f5568f9902036df20e
tommycohen <tcohen@udesa.edu.ar>
2020-05-28 19:27:30 -0300

    login

--- controllers/peliculasController.js
+++ controllers/peliculasController.js
@@ -10,6 +10,40 @@ home: (req, res) => {
 detallePelicula: (req, res) => {
     res.render('detallePelicula')
 },
+login: function(req,res){
+    var saludo = "hola"
+    res.render('login', {saludo:saludo, pagina:"login"})
+    
+},
+comparacion: function(req,res){
+    moduloLogin.chequearUsuario(req.body.email)
+    .then(resultado =>{
+        if (resultado == false){
+            console.log("El email ingresado no se encuentra en la base de datos");
+
+        }
+        else{
+            console.log("el email ingresado esta en la base de datos");
+
+            moduloLogin.buscarPorEmail(req.body.email)
+            .then(usuario =>{
+                console.log(req.body.password);
+                console.log(usuario.userPass);
+                console.log(bcrypt.compareSync(req.body.password, usuario.userPass));
+
+                if (bycrypt.compareSync(req.body.password, usuario.userPass)){
+                    var nombre = usuario.userName
+                    var idUsuario = usuario.userId
+
+                    res.send( nombre + 'te has logueado satisfactoriamente, con id:' + idUsuario)
+                }
+                else{
+                    res.send('datos invalidos')
+                }
+            })
+        }
+    })
+}
 
 }
 
