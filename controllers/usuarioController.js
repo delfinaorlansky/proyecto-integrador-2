e7d898b0c66f4ee55a377dfee76ca770a924d8ad
Victoria <vickyduque12@gmail.com>
2020-06-01 11:32:58 -0300

    buscador

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,5 +1,5 @@
 const DB = require ("../database/models");
-const OP = DB.Sequelize.Op;  
+const OP = DB.Sequelize.Op;  //operadores de sequelize que necesito incluir (tipo like)
 
 module.exports = {
 
@@ -44,23 +44,26 @@ module.exports = {
 
     //Buscador de usuario
     buscador: (req, res) => {
-    let busquedaUsuario = req.body.busqueda //aca almacenamos en una variable lo que busco el usuario
+    let busquedaUsuario = req.body.busqueda //aca almacenamos en una variable lo que busco el usuario. Uso .body porque me estoy manejando por POST.
      DB.Usuarios
      .findAll({
-          where: {
-              email: { [OP.like]: "%"+ busquedaUsuario+ "%" } 
+          where: {    //objeto literal de lo que quiero que me traiga.
+              email: { [OP.like]: "%"+ busquedaUsuario+ "%" }  //pongo el porcentaje en ambos lados asi me busca lo que coincida con lo que esta en el medio, en cualquier parte de la palabra.
           }
      })
-      .then(usuarios => {
+      .then(usuarios => {  //usuarios puede ser un nombre cualqueira. Va a ser el nombre a lo que obtenemos de respuesta del pedido asincronico. 
+        if (usuarios.lenght !=0) { 
         return res.render("usuarioIndex" , {
             listadoUsuarios: usuarios,
             tituloDePagina: "Resultado de la busqueda"  //no hago otra view, uso la misma donde liste a los usuarios.
         });
-      })
-      .catch (error => {
-        return res.send("error:" + error)
-    });
+    } else {
+        res.render("usuarioIndex" , {
+            usuarios: "No se encontraron resultados"
+        })
     }
-    
+      })
+     
+    }
    
 };
