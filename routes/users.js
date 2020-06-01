c9e8d69a691aa31dc941a838ce13e5caaed4dd85
tommycohen <tcohen@udesa.edu.ar>
2020-06-01 11:34:13 -0300

    get y post

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
 
+const usersController = require("../controllers/usuarioController");
+
+
+router.get('/list', usersController.getAll);
+
+router.get('/reviews', usersController.logUser);
+
+router.post('/reviews', usersController.confirmUser);
+
+router.get('/reviews/:id', usersController.getReviews);
+
+router.get('/reviews/edit/:id', usersController.showEdit);
+
+router.post('/reviews/edit/:id', usersController.confirmEdit);
+
+router.get('/reviews/delete/:id', usersController.deleteReview);
+
+router.post('/reviews/delete/:id', usersController.confirmDelete);
+
+
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+
 
 module.exports = router;
